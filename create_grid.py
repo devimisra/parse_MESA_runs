a348f9f45c18d98a53da5fbc1832cb81247de2ed
devimisra <devina090@gmail.com>
2025-09-30 19:38:18 +0200

    Update create_grid.py

--- create_grid.py
+++ create_grid.py
@@ -15,7 +15,7 @@ with open('joblist.txt', 'w', encoding='ascii') as f:
     job_id = 0
     for m1, m2, Porb, beta in itertools.product(m1_vals, m2_vals, porb_vals, beta_ctrl):
         job_id += 1
-        run_name = f'm1-{m1:.1f}_m2-{m2:.1f}_Porb-{Porb:.3f}_x-{xctrl:.1f}'
+        run_name = f'm1-{m1:.1f}_m2-{m2:.1f}_Porb-{Porb:.3f}_b-{beta:.1f}'
         run_dir = os.path.join(runs_dir, run_name)
         if os.path.exists(run_dir):
             shutil.rmtree(run_dir)
